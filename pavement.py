9dff56a3d8e270bb5a8be4afc1ba45637940c1a1
Chris McDonald <XWraithanX@gmail.com>
2009-12-19 05:02:52 -0800

    Added paver to the project.
    - Set to install django svn (via the github mirror)

---
+++ pavement.py
@@ -0,0 +1,22 @@
+from paver.easy import *
+import paver.virtual
+import os
+
+base = path(os.path.abspath(os.path.dirname(__file__)))
+easy_install = base + '/bin/easy_install'
+
+libraries = [
+    ('http://github.com/django/django/zipball/master', '')]
+
+options(
+    virtualenv=Bunch(
+        script_name='bootstrap.py',
+        paver_command_line=('update_libraries'),
+    ),
+)
+
+@task
+def update_libraries():
+    """Get and install libraries"""
+    for lib in libraries:
+        sh('%s %s %s' % (easy_install, lib[1], lib[0]))
